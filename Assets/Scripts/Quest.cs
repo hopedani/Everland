ab92cf312dfb2dea76783fb8b0308b87a6812420
Dan Hope <32016873+hopedani@users.noreply.github.com>
2019-09-29 22:00:15 -0400

    Added Character Controller and QuestMgr script
    Quest manager script still needs work, only basic framework has been done

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Quest
+{
+    // used for state management of quests
+     private enum QuestState
+    {
+        Uninitialized,
+        Active,
+        Complete
+    }
+
+    [SerializeField]
+    private string questName;
+    [SerializeField]
+    private string questDescription;
+    [SerializeField]
+    private string npcDialogue;
+    [SerializeField]
+    private GameObject rewardItem;
+    [SerializeField]
+    private int goldReward;
+    [SerializeField]
+    private QuestState state;
+    
+
+}
---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    // make this class a singleton... there can be only one!
+    private static QuestManager instance;
+    public static QuestManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        // ensure that only one quest manager script exists in game
+        if (!instance)
+            instance = this;
+        else
+            Destroy(this);
+
+        // used to ensure that the quest manager isn't destroyed when scenes are loaded/unloaded
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // used for easy addition/subtraction of quests in the inspector
+    public List<Quest> quests;
+
+    // used for state management of quests
+    private enum QuestState
+    {
+        Uninitialized,
+        Active, 
+        Complete
+    }
+}
