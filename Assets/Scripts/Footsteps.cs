2dc7c8bcb02ad4b875548bd4e186caf550ff20c5
Dan Hope <32016873+hopedani@users.noreply.github.com>
2019-11-04 20:54:44 -0500

    removed unused assets, phase 2 submission
    Pretty much all assets are in game, this is the submission for Phase 2

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public float stepRate = 0.4f;
+    public float stepCoolDown;
+    public AudioClip footStep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stepCoolDown -= Time.deltaTime;
+        if ((Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) && stepCoolDown < 0f)
+        {
+            GetComponent<AudioSource>().PlayOneShot(footStep, 0.9f);
+            stepCoolDown = stepRate;
+        }
+    }
+}
